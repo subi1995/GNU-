b9247dbf9c95c783cc5826de66c37697425ee572
supersubin <subi1995@naver.com>
2018-03-23 11:33:53 +0900

    Degine change ver1

--- blog/views.py
+++ blog/views.py
@@ -11,6 +11,10 @@ from django.http import HttpResponse
 from django.template import loader
 
 
+def home(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/home.html',{'posts': posts})
+
 def photo(request):
     return render(request, 'blog/photo.html')
 
